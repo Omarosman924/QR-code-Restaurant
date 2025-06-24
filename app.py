f1e91a7007ba3321d6a43c995b1c957ea0a9d3d7
Omar Osman <OmarOsman924@gmail.com>
2025-06-24 07:38:05 +0300

    add some style

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def view_orders():
     return render_template("orders.html", orders=orders)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(debug=True, host="0.0.0.0", port=8080)
